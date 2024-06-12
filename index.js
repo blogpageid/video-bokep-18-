72991520ed72ba3b88bf4efb05e56385361f1a68
blogpageid <blog.page.id@gmail.com>
2024-06-13 05:31:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,137 @@
+document.getElementById('loading').style.display = 'block';
+
+// Function to generate pagination links
+function generatePagination(totalPages, currentPage) {
+  const paginationContainer = document.getElementById('pagination');
+  paginationContainer.innerHTML = '';
+
+  // Previous page
+  const prevLi = document.createElement('li');
+  prevLi.className = 'page-item';
+  const prevLink = document.createElement('a');
+  prevLink.className = 'page-link';
+  prevLink.href = '#';
+  prevLink.textContent = 'Previous';
+  prevLink.addEventListener('click', () => fetchData(currentPage - 1));
+  prevLi.appendChild(prevLink);
+  paginationContainer.appendChild(prevLi);
+
+  // Current page
+  const currentLi = document.createElement('li');
+  currentLi.className = 'page-item active';
+  const currentLink = document.createElement('span');
+  currentLink.className = 'page-link';
+  currentLink.textContent = currentPage;
+  currentLi.appendChild(currentLink);
+  paginationContainer.appendChild(currentLi);
+
+  // Next page
+  const nextLi = document.createElement('li');
+  nextLi.className = 'page-item';
+  const nextLink = document.createElement('a');
+  nextLink.className = 'page-link';
+  nextLink.href = '#';
+  nextLink.textContent = 'Next';
+  nextLink.addEventListener('click', () => fetchData(currentPage + 1));
+  nextLi.appendChild(nextLink);
+  paginationContainer.appendChild(nextLi);
+}
+
+function handleTontonNantiClick(fileCode, singleImg,title) {
+  // Get existing list from localStorage or initialize an empty array
+  const listTonton = JSON.parse(localStorage.getItem('list_tonton')) || [];
+
+  // Create a new item to append to the list
+  const newItem = {
+    title:title,
+    filecode:fileCode,
+    image: singleImg,
+    created: new Date().toISOString(), // Store the current date and time
+  };
+
+  // Append the new item to the list
+  listTonton.push(newItem);
+
+  // Save the updated list to localStorage
+  localStorage.setItem('list_tonton', JSON.stringify(listTonton));
+
+  // Display a SweetAlert2 popup to inform the user
+  Swal.fire({
+    icon: 'SUCCESS',
+    title: 'BERHASIL DITAMBAHKAN VIDEO TERSIMPAN',
+    showConfirmButton: true,
+  });
+}
+
+
+// Function to fetch data based on random page
+function fetchRandomData(totalPages) {
+  const randomPage = Math.floor(Math.random() * totalPages) + 1; // Generate random page number
+  fetchData(randomPage);
+}
+
+// Function to fetch data based on page
+function fetchData(page) {
+  document.getElementById('loading').style.display = 'block';
+
+  fetch(`https://oakdoodserver.deno.dev/file/list?key=418098hn90f8ez0e6ssyg8&page=${page}&per_page=40`)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('loading').style.display = 'none';
+      const cardContainer = document.getElementById('card-container');
+      cardContainer.innerHTML = ''; // Clear existing cards
+      data.result.files.forEach(item => {
+        const card = document.createElement('div');
+        card.className = 'card shadow col-lg-4 col-md-3 col-sm-12';
+        card.innerHTML = `
+           <div>
+           <a href="/player.html?id=${item.file_code}">
+            <img src="${item.single_img}" 
+            style="width:100%"
+             alt="${item.title}">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">Ditonton: ${item.views}</p>
+              <p class="card-text">Di upload: ${item.uploaded}</p>
+            </div>
+            <div style="display:flex;justify-content:end">
+            
+              <a 
+              class="btn btn-danger m-3"
+              href="/player.html?id=${item.file_code}">
+              LIHAT VIDEO
+              </a>
+            </div>
+          </a>
+           <button class="btn btn-primary" style="width:100%"
+            onclick="handleTontonNantiClick('${item.file_code}', '${item.single_img}','${item.title}')">
+            SIMPAN VIDEO
+          </button>
+           </div>
+        `;
+        cardContainer.appendChild(card);
+      });
+
+      generatePagination(data.result.total_pages, page);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      document.getElementById('loading').innerHTML = 'Error loading data.';
+   
+      document.getElementById('loading').innerHTML = 'Error loading data.';
+      generatePagination(0, 0); // Display empty pagination on error
+    });
+}
+
+// Initial data fetch
+fetch(`https://oakdoodserver.deno.dev/file/list?key=418098hn90f8ez0e6ssyg8&page=1&per_page=40`)
+  .then(response => response.json())
+  .then(data => {
+    const totalPages = data.result.total_pages;
+    fetchRandomData(totalPages); // Fetch random data after getting total pages
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    document.getElementById('loading').innerHTML = 'Error loading data.';
+  });
+
---
+++ player.js
@@ -0,0 +1,104 @@
+ const urlParams = new URLSearchParams(window.location.search);
+    const fileId = urlParams.get('id');
+   const cardContainer = document.getElementById('card-container');
+
+      const card = document.createElement('div');
+        card.innerHTML = `
+          <div>
+            <iframe src="https://d0000d.com/e/${fileId}" 
+            allowfullscreen="true"
+            allowscrolling="no"
+            frameborder="0" style="width:100%;height: 400px;"></iframe>
+            <div class="container">
+              <a class="btn btn-danger mt-3" 
+              target="blank"
+              style="width:100%"
+              href="https://cuty.io/quick?token=3c2f8445e662326c2ebcd8d60&url=https://d0000d.com/d/${fileId}" target="_blank">Download Bokep Disini</a>
+              <p style="color:red;font-weight=bold">
+                Lewatin Iklan nya kalo mau download bokep bro
+              </p>
+            </div>
+          </div>
+        `;
+        cardContainer.appendChild(card);
+
+function handleTontonNantiClick(fileCode, singleImg,title) {
+  // Get existing list from localStorage or initialize an empty array
+  const listTonton = JSON.parse(localStorage.getItem('list_tonton')) || [];
+
+  // Create a new item to append to the list
+  const newItem = {
+    title:title,
+    filecode:fileCode,
+    image: singleImg,
+    created: new Date().toISOString(), // Store the current date and time
+  };
+
+  // Append the new item to the list
+  listTonton.push(newItem);
+
+  // Save the updated list to localStorage
+  localStorage.setItem('list_tonton', JSON.stringify(listTonton));
+
+  // Display a SweetAlert2 popup to inform the user
+  Swal.fire({
+    icon: 'success',
+    title: 'Berhasil ditambahkan ke TONTON NANTI',
+    showConfirmButton: true,
+  });
+}
+
+
+
+    // RELATED
+  function fetchRelated() {
+  fetch(`https://oakdoodserver.deno.dev/file/list?key=418098hn90f8ez0e6ssyg8&page=1&per_page=66`)
+    .then(response => response.json())
+    .then(data => {
+      const totalPages = data.result.total_pages;
+      const randomPage = Math.floor(Math.random() * totalPages) + 1; // Generate random page number
+      return fetch(`https://oakdoodserver.deno.dev/file/list?key=418098hn90f8ez0e6ssyg8&page=${randomPage}&per_page=66`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      const cardContainer = document.getElementById('related');
+      cardContainer.innerHTML = ''; // Clear existing cards
+      data.result.files.forEach(item => {
+        const card = document.createElement('div');
+        card.className = 'card col-lg-4 col-md-3 col-sm-12 ';
+        card.innerHTML = `
+          <div class="shadow-lg">
+           <a href="/player.html?id=${item.file_code}"
+           style="text-decoration:none;color:black"
+           >
+            <img src="${item.single_img}" class="card-img-top" alt="${item.title}">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">Views: ${item.views}</p>
+              <p class="card-text">Uploaded: ${item.uploaded}</p>
+            </div>
+            <div style="display:flex;justify-content:end">
+              <a 
+              class="btn btn-danger m-3"
+              href="/player.html?id=${item.file_code}">
+              LIHAT VIDEO
+              </a>
+            </div>
+          </a>
+          <button class="btn btn-primary" style="width:100%"
+            onclick="handleTontonNantiClick('${item.file_code}', '${item.single_img}','${item.title}')">
+            SIMPAN VIDEO
+          </button>
+          </div>
+        `;
+        cardContainer.appendChild(card);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      document.getElementById('loading').innerHTML = 'Error loading data.';
+      document.getElementById('loading').innerHTML = 'Error loading data.';
+    });
+}
+
+fetchRelated()
